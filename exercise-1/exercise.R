74266b4b375213a777af58aac388c8f016b36a4c
Katherine <kathlu@uw.edu>
2017-05-17 14:41:49 -0700

    made progress

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,9 +1,17 @@
 # Exercise 1: Loading functions
 
 # Set your directory
-
+setwd("~/Desktop/INFO-201/m18-shiny/exercise-1")
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source("./scripts/BuildScatter.r")
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+BuildScatter(data = iris, 
+             varX = 'Sepal.Width', 
+             varY = 'Sepal.Length',
+             varColor = 'Species', 
+             title = 'Iris Dataset', 
+             titleX = 'Sepal Width', 
+             titleY = 'Sepal Length'
+)
--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,11 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+BuildScatter <- function(data, varX, varY, varColor, title = "Title", titleX = "X Title", titleY = "Y Title") {
+  p <- ggplot(data = data) +
+    geom_point(mapping = aes(x = data[,varX], y=data[,varY], color = data[,varColor])) + 
+    labs(title = title, x = titleX, y = titleY)
+  return(p)
+}
+
